7808c7296c29bb2e41b348e5e193a443fb81a8a7
hhachich <h.hachichou@gmail.com>
2024-06-25 09:24:21 +0200

    test du fetch de l'api

--- src/ApiMovie.js
+++ src/ApiMovie.js
@@ -1,20 +1,38 @@
-import React from 'react';
-
 const API_KEY= process.env.REACT_APP_API_KEY
 const API_URL= process.env.REACT_APP_API_URL
 
+const fetchMovies = async (endpoint) => {
+    return await fetch(
+      `${API_URL}${endpoint}?language=fr-FR`, {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${API_KEY}`,
+        },
+      }
+      
+    ).then((response) => response.json())
+  }
 
-const ApiMovie = () => {
-    
-    
-    
-    return (
-        <div>
-            <h1>ApiMovie</h1>
-            <p>API_KEY = {API_KEY}</p>
-            <p>API_URL = {API_URL}</p>
-        </div>
-    );
-};
 
-export default ApiMovie;
+export default {
+    getHomeMovies: async () => {
+        return [
+            {
+              slug: "top-rated",
+              title: "Mieux notés",
+              items: await fetchMovies("movie/top_rated"),
+            },
+            {
+                slug: "upcoming",
+                title: "Prochaines sorties",
+                items: await fetchMovies("movie/upcoming"),
+            }, 
+            {
+                slug: "tvpopular",
+                title: "Séries populaires",
+                items: await fetchMovies("tv/popular"),
+            },
+        ]
+    }   
+}
+
--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useEffect,useState  } from 'react';
 import ApiMovie from './ApiMovie';
 import './App.css';
 
 function App() {
+  const [moviesList, setMoviesList] = useState([])
+
+
+  useEffect(() => {
+    const loadAllMovies = async () => {
+      let list = await ApiMovie.getHomeMovies()
+      setMoviesList(list)
+      console.log(list)
+    }
+
+    loadAllMovies()
+  }, [])
+
   return (
     <div className="App">
-        <h1>APP</h1> 
-        <ApiMovie/>
+      <section className="lists">
+        {moviesList.map((item, key) => (
+          <p key={key}  >{item.title}</p>
+        ))}
+      </section>        
     </div>
   );
 }
